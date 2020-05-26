cd5e64f90d5618bd058e205e6dc572ec568a6e16
Miqueias Mesquita <miqueiasdesenvolvedor@gmail.com>
2020-05-26 20:13:50 -0300

    comunicaçao entre componentes

---
+++ comunicacao/src/components/UsuarioInfo.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="componente">
+        <h2>As Informações de Usuário</h2>
+        <p>Vários detalhes...</p>
+        <p>Nome do Usuário: <strong>{{nome}}</strong></p>
+         <p>Idade do Usuário <strong>{{ idade }}</strong></p>
+        <button @click="reiniciarNome">Reiniciar Nome</button>
+        <button @click="reiniciarFn">Reiniciar nome (callback)</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        nome: {
+            type: String,
+            // required: true,
+            // default: function(){
+            //     return Array(10).fill(0).join(',')
+            // }
+            default: 'Anonimo'
+        },
+        reiniciarFn: Function,
+        idade: Number
+    },
+    methods: {
+        reiniciarNome()
+        {
+            this.nome = "Pedro"
+            this.$emit('nomeMudou', this.nome)
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .componente {
+        flex: 1;
+        background-color: #ec485f;
+        color: #fff;
+    }
+</style>
